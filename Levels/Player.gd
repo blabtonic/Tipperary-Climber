7dfbcd51fe04b5c946272d6d52616415ba39c512
Trystan Hendricks <dolkhound@gmx.com>
2020-04-09 18:09:42 -0400

    adding gravity

--- Levels/Player.gd
+++ Levels/Player.gd
@@ -5,7 +5,10 @@ var motion = Vector2()
 var speed = 2 * 60
 
 func _physics_process(delta):
+	#Gravity
 	motion.y += 10
+	
+	# Player Movement
 	if Input.is_action_pressed('ui_right'):
 		motion.x = 100
 	elif Input.is_action_pressed('ui_left'):
