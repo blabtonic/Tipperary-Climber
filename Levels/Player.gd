3c367053d32ae9d205e85738538e33802caead30
Trystan Hendricks <dolkhound@gmx.com>
2020-04-09 22:00:08 -0400

    creating smooth movement added friction boolean

--- Levels/Player.gd
+++ Levels/Player.gd
@@ -1,34 +1,44 @@
 extends KinematicBody2D
 
 const UP = Vector2(0,-1)
-const GRAVITY = 30
-const SPEED = 120
-const JUMP_HEIGHT = -600
+const ACCLELERATION = 60
+const GRAVITY = 15
+const MAX_SPEED = 120
+const JUMP_HEIGHT = -300
 
 var motion = Vector2()
 
 
 func _physics_process(delta):
-	#Gravity
-	motion.y += 10
+	# Gravity
+	motion.y += GRAVITY
+	
+	# Friction
+	var friction = false
 	
 	# Player Movement
 	if Input.is_action_pressed('ui_right'):
-		motion.x = 100
+		motion.x = min(motion.x+ACCLELERATION, MAX_SPEED)
 		$Sprite.flip_h = false
 		$Sprite.play('Run')
 	elif Input.is_action_pressed('ui_left'):
-		motion.x = -100
+		motion.x = max(motion.x-ACCLELERATION, -MAX_SPEED)
 		$Sprite.flip_h = true
 		$Sprite.play('Run')
 	else:
-		motion.x = 0
 		$Sprite.play('Idle')
-	# Jump added
+		friction = true
+		motion.x = lerp(motion.x, 0, 0.2)
+	
 	if is_on_floor():
 		if Input.is_action_just_pressed('ui_up'):
-			motion.y = -300
+			motion.y = JUMP_HEIGHT
+		if friction == true:
+			#Linearly interpolates between two values by a normalized value lerp()
+			motion.x = lerp(motion.x, 0, 0.2) 
 	else:
 		$Sprite.play('Jump')
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.5)
 	
 	motion = move_and_slide(motion, UP)
