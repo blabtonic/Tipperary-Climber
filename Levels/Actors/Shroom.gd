befd9bbea4ec577d6547edc3d393e9066acf2777
Trystan Hendricks <dolkhound@gmx.com>
2020-04-10 18:34:42 -0400

    playing with raycast to create bounceArea

--- Levels/Actors/Shroom.gd
+++ Levels/Actors/Shroom.gd
@@ -1,11 +1,10 @@
 extends KinematicBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var areaBouncy
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
+
+func _physics_process(delta):
+	pass
