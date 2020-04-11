4d9397af8a2742ee9c194b88e75efdcef22201d7
Trystan Hendricks <dolkhound@gmx.com>
2020-04-10 20:24:07 -0400

    bounce pad is working(check logic)

--- Levels/Player.gd
+++ Levels/Player.gd
@@ -5,8 +5,10 @@ const ACCLELERATION = 60
 const GRAVITY = 15
 const MAX_SPEED = 85
 const JUMP_HEIGHT = -300
+const BOUNCER = 6 # need to look into bounce logic
 
 var motion = Vector2()
+onready var bounce_raycasts = $BounceRayCast
 
 
 func _physics_process(delta):
@@ -29,6 +31,7 @@ func _physics_process(delta):
 		$Sprite.play('Idle')
 		friction = true
 	
+	# Jumping Physics
 	if is_on_floor():
 		if Input.is_action_just_pressed('ui_up'):
 			$JumpSound.play()
@@ -36,6 +39,7 @@ func _physics_process(delta):
 		if friction == true:
 			#Linearly interpolates between two values by a normalized value lerp()
 			motion.x = lerp(motion.x, 0, 0.2) 
+			_check_bounce(delta)
 	else:
 		if motion.y < 0:
 			$Sprite.play('Jump')
@@ -45,3 +49,14 @@ func _physics_process(delta):
 			motion.x = lerp(motion.x, 0, 0.5)
 	
 	motion = move_and_slide(motion, UP)
+
+func _check_bounce(delta):
+	if motion.y > 0:
+		for raycast in bounce_raycasts.get_children():
+			raycast.cast_to = Vector2.DOWN * motion * delta + Vector2.DOWN
+			raycast.force_raycast_update()
+			if raycast.is_colliding() && raycast.get_collision_normal() == Vector2.UP:
+				motion.y = (raycast.get_collision_point() - raycast.global_position - 
+				Vector2.DOWN).y / delta * BOUNCER
+				raycast.get_collider().entity.call_deferred('bouncing', self)
+				break
