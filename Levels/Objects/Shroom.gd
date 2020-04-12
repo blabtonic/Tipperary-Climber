48372320179d91bfcebfb35108d2ef26114c490e
Trystan Hendricks <dolkhound@gmx.com>
2020-04-11 20:24:03 -0400

    moved scenes

--- Levels/Objects/Shroom.gd
+++ Levels/Objects/Shroom.gd
@@ -1,6 +1,5 @@
 extends KinematicBody2D
 
-
 var run_speed = 350
 var jump_speed = -500
 var gravity = 2000
@@ -10,11 +9,4 @@ onready var animation = get_node('Sprite')
 
 func _ready():
 	#need signal
-	get_node('Sprite').connect('body_enter', self, '_enter')
-
-func _enter(body):
-	if !animation.is_playing():
-		animation.play('Bounce')
-		
-		if body.has_method('bump'):
-			body.bump()
+	pass
