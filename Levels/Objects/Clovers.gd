8b9e82f475b47a3d29ee44c66e5596830881d46b
Trystan Hendricks <dolkhound@gmx.com>
2020-04-11 20:08:12 -0400

    clover updates

--- Levels/Objects/Clovers.gd
+++ Levels/Objects/Clovers.gd
@@ -1,6 +1,10 @@
 extends Area2D
 
-onready var collect_animation = $AnimationPlayer
+onready var collect_animation = $AnimationPlayer 
+
+func _ready():
+	$AnimationPlayer.play('Clover_Idle')
 
 func _on_Clover_body_entered(body):
+	$CollectSound.play()
 	collect_animation.play('fade_out')
